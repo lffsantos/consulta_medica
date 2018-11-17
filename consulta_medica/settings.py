a8945af45ec3e0d31867c324011ae313b8e319ca
Lucas <lffsantos@gmail.com>
2018-11-17 00:47:29 -0200

    test

--- consulta_medica/settings.py
+++ consulta_medica/settings.py
@@ -124,7 +124,3 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
